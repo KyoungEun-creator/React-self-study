2af00a535a8e1d78896f5f855ef059a320d6de5c
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-08 23:25:14 +0100

    Day4
    state 만들기

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
+import Subject from "./components/Subject";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
-import Subject from "./components/Subject";
 import './App.css';
 
-
-
-
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web'},
+      contents:[
+        {id:1, title:"HTML", desc:"HTML is HyperText Markup Language."},
+        {id:2, title:"CSS", desc:"CSS is for design."},
+        {id:3, title:"JAVASCRIPT", desc:"JAVASCRIPT is for interaction."}
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web"></Subject>
-        <Subject title="React" sub="for UI"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
 }
-
 
 export default App;
