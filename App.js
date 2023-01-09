ef4ca1f798386d2dacf0cf66bf40f2357cbddad5
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-09 23:22:39 +0100

    Day5
    이벤트 설치: onClick={}
    이벤트 설치 시 기본적인 동작(reload) 못하게 막기: e.preventDefault()
    이벤트를 실시하면 state 변경되게 하기: bind함수, set함수

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode:'welcome',
       subject:{title:'WEB', sub:'World Wide Web'},
+      welcome:{title:'Welcome', desc:'Hello, React'},
       contents:[
         {id:1, title:"HTML", desc:"HTML is HyperText Markup Language."},
         {id:2, title:"CSS", desc:"CSS is for design."},
@@ -17,11 +19,29 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){ {/* 링크를 클릭했을 때 실행되도록 */}
+            e.preventDefault();                 {/* 기본적인 동작:reload 못하도록 */}
+            //  this.state.mode = 'welcome'
+            this.setState({
+              mode:'welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1> {/* 컴포넌트 자체를 지목하도록 .bind */}
+          {this.state.subject.sub}
+        </header>  
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
