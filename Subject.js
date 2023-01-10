31abbe6863f30ccb12c9665457acc53079a10271
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-10 03:46:27 +0100

    Day6
    나만의 이벤트 함수가 포함된 component 만들기

--- Subject.js
+++ Subject.js
@@ -4,7 +4,10 @@ class Subject extends Component {
     render () {
       return (        
         <header>
-          <h1><a href="/">{this.props.title}</a></h1>
+          <h1><a href="/" onClick={function(e){
+            e.preventDefault();                 {/* 링크 클릭했을 때 페이지가 바뀌는 것 방지 */}
+            this.props.onChangePage();          {/* 함수 호출 */}
+          }.bind(this)}> {this.props.title} </a></h1>
           {this.props.sub}
         </header>  
       );
