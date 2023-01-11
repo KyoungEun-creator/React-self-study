6e18e98e6c8bc4b7a66d3ae22cb5ccf0d4061603
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-11 23:10:54 +0100

    Day7
    this.setState({mode: }) 통해 mode 변경 시
    component 교체되도록
    from 'ReadContent' to 'CreateContent'

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Subject from "./components/Subject";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import Control from "./components/Control";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
 import './App.css';
 
 class App extends Component {
@@ -20,10 +22,11 @@ class App extends Component {
     }
   }
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>  //기존의 ReadContent를 _article이라는 변수에 줌 
     } else if (this.state.mode === 'read') {
       var i = 0;
       while(i<this.state.contents.length) {
@@ -35,16 +38,19 @@ class App extends Component {
         }
         i+=1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent> //mode가 'welcome'이든 'article이든 기존 ReadContent
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent></CreateContent>
     }
     return (
       <div className="App">
         <Subject 
-        title={this.state.subject.title} 
-        sub={this.state.subject.sub}
-        onChangePage={function(){       {/* 내가 만든 이벤트 함수 */}
-          this.setState({mode:'welcome'});
-        }.bind(this)}
-        ></Subject>
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function(){       {/* 내가 만든 이벤트 함수 */}
+            this.setState({mode:'welcome'});
+          }.bind(this)}>
+        </Subject>
         <TOC onChangePage={function(id){
           this.setState({
             mode:'read', 
@@ -53,7 +59,12 @@ class App extends Component {
         }.bind(this)} 
         data={this.state.contents}>
         </TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode: _mode});
+          }.bind(this)}>
+        </Control>
+        {_article}
       </div>
     );
   }
