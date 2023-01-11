4362d6b6fa26f580d930b776c0069150d50c0b2f
Joe <80874074+KyoungEun-creator@users.noreply.github.com>
2023-01-12 00:41:34 +0100

    Day7-1
    CreateContent의 form 만들기

--- CreateContent.js
+++ CreateContent.js
@@ -4,9 +4,15 @@ class CreateContent extends Component {
     render () {
       return (
         <article>
-          <h2>Create</h2>
-          <form>
-            
+          <h2>Create</h2>                                                      {/* action:사용자가 입력한 정보 전송할 장소, method:url에 노출 안 되도록*/}
+          <form action="/create_process" method="post"                         
+            onSubmit={function(e){
+              e.preventDefault();
+              alert('submit')
+            }.bind(this)}>
+            <p><input type="text" name="title" placeholder="title"></input></p>
+            <textarea name="desc" placeholder="description"></textarea>         {/* 입력할 텍스트가 여러 줄일 때 사용 */}
+            <p><input type="submit"></input></p>
           </form>
         </article>
       );
